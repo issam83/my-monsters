0652d3d76d206eb455bed26edfa01131b3935e60
issam83 <elagrebiissam@gmail.com>
2020-03-25 01:30:16 +0100

    Updates

---
+++ precache-manifest.d25c0bedf2c62674c2d552bc5c48296c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e1eaac922bbded4baf9c38932519f2b3",
+    "url": "/my-monsters/index.html"
+  },
+  {
+    "revision": "36e2d7c86e76f25dc46b",
+    "url": "/my-monsters/static/css/main.5d7c4b5b.chunk.css"
+  },
+  {
+    "revision": "1e8d71802ca5d0371cbc",
+    "url": "/my-monsters/static/js/2.eaad7329.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/my-monsters/static/js/2.eaad7329.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "36e2d7c86e76f25dc46b",
+    "url": "/my-monsters/static/js/main.19a5a6cc.chunk.js"
+  },
+  {
+    "revision": "d6df6b4e84d7bb3b1e26",
+    "url": "/my-monsters/static/js/runtime-main.6ee849ed.js"
+  }
+]);
